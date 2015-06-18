9c368e04a08fec0550c06ea6c018efc92274d7ed
Xeronel <ripster1121@gmail.com>
2015-06-18 10:58:40 -0400

    Revert "bot now moves people to #help when they type !help. Also auto auths with operserv and nickserv"
    This reverts commit 17c34d07b40330739dd92fc5c95c5a4850ff6dcd.

--- bot.py
+++ bot.py
@@ -1,47 +1,11 @@
-from twisted.internet import reactor, protocol, ssl
-from twisted.words.protocols import irc
+__author__ = 'ripster'
 
-class Client(irc.IRCClient):
-    nickname = "bot"
-    operpassword = "ChingChongMotherFucker"
-    nickservpass = "mehmehmeh"
-    email = "legacy@ratio.ninja"
-    registered = "No"
+from twisted.internet import reactor, ssl
+from modules import irc
 
-    def signedOn(self):
-        self.join(self.factory.channel)
-	if self.registered == "No":
-	    self.msg("NickServ", "register %s %s" % (self.nickservpass, self.email))
-	    self.registered = "Yes"
-	elif self.registered == "Yes":
-	    self.msg("NickServ", "identify %s" % self.nickservpass)
-        self.sendLine("oper %s %s" % (self.nickname, self.operpassword))
-        self.msg("OperServ", "login %s" % self.operpassword)
-    
-    def privmsg(self, user, channel, message):
-        user = user.split("!", 1)[0]
-        if message.startswith("!help"):
-            self.msg(channel, "%s: Help is this way :)" % user)
-            self.sendLine("sajoin %s #help" % user)
 
-class Factory(protocol.ClientFactory):
-    def __init__(self, channel):
-        self.channel = channel
-    
-    def buildProtocol(self, addr):
-       	proto = Client()
-        proto.factory = self
-        return proto
-    
-    def clientConnectionLost(self, connector, reason):
-        # Try to reconnect if disconnected.
-        connector.connect()
-    
-    def clientConnectionFailed(self, connector, reason):
-        reactor.stop()
-
-network = "irc.ratio.ninja"
-port = 6697
-channel = "rationinja"
-reactor.connectSSL(network, port, Factory(channel), ssl.ClientContextFactory())
+reactor.connectSSL('irc.ratio.ninja',
+                   6697,
+                   irc.IRCFactory('#rationinja', 'RipBot', reactor),
+                   ssl.ClientContextFactory())
 reactor.run()
