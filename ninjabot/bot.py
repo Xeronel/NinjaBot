b9756a17eba70fa178f2ee0f44d22d4686649577
Xeronel <ripster1121@gmail.com>
2015-06-26 17:52:57 -0400

    Added method to get user modes

--- ninjabot/bot.py
+++ ninjabot/bot.py
@@ -9,15 +9,34 @@ class IRCClient(irc.IRCClient):
     def __init__(self, nickname, channels):
         self.nickname = nickname
         self.channels = channels
+        self.usermodes = {}
 
     def signedOn(self):
         for channel in self.channels:
             self.join(channel)
 
+    def joined(self, channel):
+        self.getUserModes('#' + channel)
+
     def privmsg(self, user, channel, message):
+        print('User: %s' % user)
+        print('Channel: %s' % channel)
+        print('Message: %s' % message)
         if user.startswith('Ripster!') and message == '!stop':
             print('%s issued stop command.' % user.split('!')[0])
             self.quit()
+
+    def modeChanged(self, user, channel, set, modes, args):
+        print('Mode set %s %s applied to %s in channel %s' % (set, modes, user, channel))
+
+    def irc_RPL_NAMREPLY(self, prefix, params):
+        channel = params[2]
+        user_list = params[3]
+        self.usermodes[channel] = user_list.strip().split(' ')
+        print(self.usermodes)
+
+    def getUserModes(self, channel):
+        self.sendLine('names #%s' % channel)
 
 
 class IRCFactory(protocol.ClientFactory):
