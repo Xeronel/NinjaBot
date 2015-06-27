274a0aa54df2e92e0909b5a7f39b8f5a1d588fa1
Xeronel <ripster1121@gmail.com>
2015-06-27 17:39:31 -0400

    Accept user, channel, and args as command params

--- ninjabot/commands/commands.py
+++ ninjabot/commands/commands.py
@@ -11,5 +11,5 @@ class BaseCommand(object):
         # Groups allowed to run the command
         self.allow = []
 
-    def execute(self, user, channel):
+    def execute(self, user, channel, args):
         pass
