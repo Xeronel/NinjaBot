2936fd916dad341d51f62a09b62fddfc8fc19a38
Xeronel <ripster1121@gmail.com>
2015-06-27 16:13:37 -0400

    Get list of users and their modes on join

--- ninjabot/bot.py
+++ ninjabot/bot.py
@@ -6,10 +6,11 @@ import config
 
 
 class IRCClient(irc.IRCClient):
-    def __init__(self, nickname, channels):
+    def __init__(self, nickname, channels, modes):
         self.nickname = nickname
         self.channels = channels
-        self.usermodes = {}
+        self.supp_modes = modes  # Modes supported by the IRC server
+        self.users = {}  # User dict organized by channel and mode
 
     def signedOn(self):
         for channel in self.channels:
@@ -31,21 +32,38 @@ class IRCClient(irc.IRCClient):
 
     def irc_RPL_NAMREPLY(self, prefix, params):
         channel = params[2]
-        user_list = params[3]
-        self.usermodes[channel] = user_list.strip().split(' ')
-        print(self.usermodes)
+        user_list = params[3].strip().split(' ')
+
+        if channel not in self.users:
+            self.users[channel] = {}
+
+        if '*' not in self.users[channel]:
+            self.users[channel]['*'] = []
+
+        for user in user_list:
+            mode = user[0]
+
+            if mode in self.supp_modes:
+                if mode not in self.users:
+                    self.users[channel][mode] = []
+                self.users[channel][mode].append(user[1:])
+            else:
+                self.users[channel]['*'].append(user)
+
+        print(self.users)
 
     def getUserModes(self, channel):
         self.sendLine('names #%s' % channel)
 
 
 class IRCFactory(protocol.ClientFactory):
-    def __init__(self, channels, nickname):
-        self.channels = channels
+    def __init__(self, nickname, channels, modes):
         self.nickname = nickname
+        self.channels = channels
+        self.modes = modes
 
     def buildProtocol(self, address):
-        proto = IRCClient(self.nickname, self.channels)
+        proto = IRCClient(self.nickname, self.channels, self.modes)
         proto.factory = self
         return proto
 
@@ -64,12 +82,14 @@ def run():
     if irc_cfg.network.ssl:
         reactor.connectSSL(irc_cfg.network.address,
                            irc_cfg.network.port,
-                           IRCFactory(irc_cfg.channels,
-                                      irc_cfg.user.nickname),
+                           IRCFactory(irc_cfg.user.nickname,
+                                      irc_cfg.channels,
+                                      irc_cfg.modes),
                            ssl.ClientContextFactory())
     else:
         reactor.connectTCP(irc_cfg.irc.network.address,
                            irc_cfg.irc.network.port,
-                           IRCFactory(irc_cfg.channels,
-                                      irc_cfg.user.nickname))
+                           IRCFactory(irc_cfg.user.nickname,
+                                      irc_cfg.channels,
+                                      irc_cfg.modes))
     reactor.run()
