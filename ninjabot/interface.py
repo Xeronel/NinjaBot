d685fd5a91c6dd317807ed5e27846c3099c1f90f
Xeronel <ripster1121@gmail.com>
2015-07-07 10:35:15 -0400

    Removed old command interface

--- ninjabot/interface.py
+++ ninjabot/interface.py
@@ -39,39 +39,3 @@ class EventHandler(BaseEvents):
         elif isinstance(message, list):
             for msg in message:
                 self.irc.sendLine(msg)
-
-
-def run_command(user, mode, channel, message):
-    """
-    :param irc: IRC Client
-    :param user: Executor of the command
-    :param mode: Mode of the user (~, &, @, %, *)
-    :param channel: Channel the command was run in
-    :param message: The users message
-    :return: True if command was run, else False
-    """
-    msg = message.split(' ')
-    cmd = msg[0][1:].lower()
-    is_cmd = True if cmd in commands else False
-
-    if is_cmd:
-        try:
-            result = commands[cmd].execute(user, mode, channel, msg)
-        except Exception as e:
-            result = 'PRIVMSG %s :Error running %s command (%s)' % (channel, cmd, e.message)
-    else:
-        result = None
-
-    try:
-        if isinstance(result, list) or isinstance(result, tuple):
-            for i in result:
-                irc.sendLine(i)
-            return True
-        elif isinstance(result, str):
-            irc.sendLine(result)
-            return True
-        else:
-            return False
-    except Exception as e:
-        irc.sendLine('PRIVMSG %s :Error running %s command (%s)' % (channel, message, e.message))
-        return False
